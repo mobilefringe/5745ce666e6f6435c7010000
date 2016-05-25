4ce67e5720561cf69b4b09a79d16254f0cf88eb4
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-25 13:45:25 -0400

    Long Beach Towne Center 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The District at Green Valley Ranch",
                     "default_image" : "http://assets.codecloudapp.com/sites/5745ce666e6f6435c7010000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "da5cf03899eb49a496424d9a76bafa0d"
+                    "instagram" : "196585874"
                 }
