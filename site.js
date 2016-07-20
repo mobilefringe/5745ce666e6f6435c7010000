0f9ef4e3e1fbef8120cc19dcfff248f4601c3674
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-07-20 11:16:33 -0400

    Long Beach Towne Center 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Long Beach Towne Center",
                     "default_image" : "http://assets.codecloudapp.com/sites/5745ce666e6f6435c7010000/image/jpeg/undefined/lbtc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196585874"
+                    "social_feed" : "http://longbeach.mallmaverick.com/api/v2/longbeach/social.json"
                 }
